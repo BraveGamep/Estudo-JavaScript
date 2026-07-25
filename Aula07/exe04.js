e23457fb8788cabab9d6390b97b4d4686cd348a3
BraveGamep <Brave_gamePLay@outlook.com>
2026-07-25 11:51:31 -0300

    Add files via upload

---
+++ Aula07/exe04.js
@@ -0,0 +1,53 @@
+//Declaração de variável
+var n1 = 5
+var n2 = 2
+//operação aritméticas
+let soma = n1 + n2
+let subtracao = n1 - n2
+let multiplicacao = n1 * n2
+let divisao = n1 / n2
+let resto = n1 % n2
+let potencia = n1 ** n2
+//Mostrar resultado
+document.write("os valores sao 5 e 2 <br>")
+document.write(`Soma é ${soma} <br>`)
+document.write(`Subtração é ${subtracao} <br>`)
+document.write(`Multiplicação é ${multiplicacao} <br>`)
+document.write(`Divisão é ${divisao} <br>`)
+document.write(`O resto é ${resto} <br>`)
+document.write(`A potência é ${potencia}`)
+
+//Atribuição Simples 
+
+let a = 5+3
+let b = a-2
+let c = 5 * b ** 2
+let d = 10 - a / 2
+let e = 6*2 / d
+let f = b % e + 4 / e
+
+// Auto-atribuições
+
+let n = 3
+n = n+5 // 8
+n = n-2 // 6
+n = n*4 // 24
+n = n/2 // 12
+n = n%5 // 2
+n = n**3 // 8
+
+//Simplificando
+let m = 3
+m += 5
+m -= 2
+m *= 4
+m /= 2
+m %= 5
+m **= 3
+
+//Incremento
+x = 5
+x++
+
+y = 5
+y--
