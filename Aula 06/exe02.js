e7d4f3cdcb95a966f8e766378de07c113fc58dee
BraveGamep <Brave_gamePLay@outlook.com>
2026-07-25 12:23:59 -0300

    Add files via upload

---
+++ Aula 06/exe02.js
@@ -0,0 +1,7 @@
+//exer 02 aula6
+
+var numero1 = Number(window.prompt("Digite um número"))
+var numero2 = Number(window.prompt("Digite outro número"))
+var soma = numero1 + numero2
+
+window.alert("A soma deu: " + soma)
