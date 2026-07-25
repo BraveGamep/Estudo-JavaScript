d8b9b661177cfe4111c6fca2dc9032f051a9d594
BraveGamep <Brave_gamePLay@outlook.com>
2026-07-25 11:48:23 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+/* window.alert("JavaScript")
+window.confirm("Gostaria de abrir outra instância do Roblox?")
+window.prompt("Gostaria de mudar o idioma?") */
+
+//aula 05
+var nome = window.prompt("Qual o seu nome")
+var idade = "25"
+
+//aula 06
+window.alert("Prazer, " + nome)//+ concatena string com variável
+
+//exe02
+
+var numer
